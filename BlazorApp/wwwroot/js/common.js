17e3ec2be0e32e555e22df8fde31107bb4e9a2dd
Tasnim-Ansari <Tasnim-Ansari@users.noreply.github.com>
2023-07-28 18:34:13 +0530

    Added validation msg for empty Username and password Added popup for Invalid credentials Made License component as the default page

---
+++ BlazorApp/wwwroot/js/common.js
@@ -0,0 +1,16 @@
+﻿window.ShowAlert = (type, message) => {
+    if (type == "success") {
+        Swal.fire(
+            'Success Notification!',
+            'Success',
+            'success'
+        )
+    }
+    else {
+        Swal.fire(
+            'Failure Notification!',
+            message,
+            'error'
+        )
+    }
+}
