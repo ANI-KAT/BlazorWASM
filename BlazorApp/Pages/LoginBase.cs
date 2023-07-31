2d7707296eb4477abb83c0928b6c19388aa145e2
Alamgeer Ashraf <alamgeer06@gmail.com>
2023-07-31 15:46:22 +0530

    Added login page with integrated api.

---
+++ BlazorApp/Pages/LoginBase.cs
@@ -0,0 +1,59 @@
+﻿using BlazorApp.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorApp.Pages
+{
+    public class LoginBase : ComponentBase
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string UserNmErrorMsg { get; set; } = string.Empty;
+        public string PasswdErrorMsg { get; set; } = string.Empty;
+        public bool ShowSpinner { get; set; } = false;
+
+        [Inject]
+        public NavigationManager NavManager { get; set; }
+
+        [Inject]
+        public IJSRuntime _jsRuntime { get; set; }
+
+
+        [Inject]
+        public IUserService UserService { get; set; }
+
+        private DateTime dt = new DateTime(2023, 8, 30, 11, 0, 0);
+
+        public async Task OnValidLicense()
+        {
+            string msg = string.Empty;
+            UserNmErrorMsg = PasswdErrorMsg = string.Empty;
+            if (!string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password))
+            {
+                ShowSpinner = true;
+                var result = await UserService.Login(new UserRequest()
+                {
+                    email = Username,
+                    password = Password
+                });
+                ShowSpinner = false;
+
+                if (result == null)
+                    await _jsRuntime.InvokeVoidAsync("ShowAlert", "error", "Server not responding.");
+                else
+                    NavManager.NavigateTo($"/license/{result.access_token}");
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(Username))
+                {
+                    UserNmErrorMsg = "Username cannot be blank";
+                }
+                if (string.IsNullOrWhiteSpace(Password))
+                {
+                    PasswdErrorMsg = "Password cannot be blank";
+                }
+            }
+        }
+    }
+}
--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp;
+using BlazorApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Add Http Service
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.escuelajs.co/") });
+builder.Services.AddScoped<IUserService, UserService>();
 
 await builder.Build().RunAsync();
